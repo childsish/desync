40f5f27a0cc7ca8bc3bef38223c745a605154b30
Liam Childs <liam.h.childs@gmail.com>
2022-02-12 13:20:49 +0100

    add nested integration test

---
+++ tests/integration/test_nested.py
@@ -0,0 +1,22 @@
+import unittest
+
+from swee import desync
+
+
+def inner(item):
+    return item + 1
+
+
+@desync
+def outer1(item):
+    return inner(item)
+
+
+@desync
+def outer2(item):
+    return outer1(item)
+
+
+class TestNested(unittest.TestCase):
+    def test_nested(self):
+        self.assertEqual(1, outer2(0))
