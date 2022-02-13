40948fdb410338e8126db937bef2fdac671fc5fa
Liam Childs <liam.h.childs@gmail.com>
2022-02-13 20:49:47 +0100

    add basic unit test

---
+++ tests/unit/test_cache.py
@@ -0,0 +1,18 @@
+from swee.cache import Cache
+from unittest import TestCase
+
+
+class TestCache(TestCase):
+    def test_outputs(self):
+        cache = Cache({
+            0: {0: 0},
+        })
+
+        self.assertTrue(cache.has_outputs(0, 0))
+        self.assertFalse(cache.has_outputs(0, 1))
+        self.assertFalse(cache.has_outputs(1, 0))
+        self.assertEqual(0, cache.get_outputs(0, 0))
+
+        cache.set_outputs(0, 1, 0)
+        self.assertTrue(cache.has_outputs(0, 1))
+        self.assertEqual(0, cache.get_outputs(0, 1))
---
+++ tests/unit/test_version.py
@@ -0,0 +1,39 @@
+from swee.version import Version
+from unittest import TestCase
+
+
+class TestVersion(TestCase):
+    def test_no_update(self):
+        old_version = Version(1, (0, 0))
+        new_version = Version(1, (0, 0))
+        new_version.set_old_version(old_version)
+
+        self.assertEqual('1.0', str(new_version))
+
+    def test_minor_update(self):
+        old_version = Version(1, (0, 0))
+        new_version = Version(1, (0, 1))
+        new_version.set_old_version(old_version)
+
+        self.assertEqual('1.1', str(new_version))
+
+    def test_major_update(self):
+        old_version = Version(1, (0, 0))
+        new_version = Version(2, (0, 0))
+        new_version.set_old_version(old_version)
+
+        self.assertEqual('2.0', str(new_version))
+
+    def test_update_both(self):
+        old_version = Version(1, (0, 0))
+        new_version = Version(2, (0, 0))
+        new_version.set_old_version(old_version)
+
+        self.assertEqual('2.0', str(new_version))
+
+    def test_input_changes(self):
+        old_version = Version(1, (0, 0))
+        new_version = Version(1, (0, 0))
+        new_version.set_old_version(old_version)
+
+        self.assertEqual('1.0', str(new_version))
