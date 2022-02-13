7b2b166d088a754adf951ac17744357fa1460039
Liam Childs <liam.h.childs@gmail.com>
2022-02-13 22:15:15 +0100

    add setup.y

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='desync',
+    version='1.0.0',
+    url='https://github.com/childsish/desync',
+    author='Liam Childs',
+    author_email='liam.h.childs@gmail.com',
+    description='Desynchronise functions',
+    packages=find_packages(),
+    install_requires=[],
+)
